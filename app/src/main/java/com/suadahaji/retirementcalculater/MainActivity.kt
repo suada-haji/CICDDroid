5f1aecb7dc75d555e43f05b813f135b0bb9b2369
Suada Haji <suadahaji@gmail.com>
2020-08-15 10:15:47 +0300

    Send event properties

--- app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
+++ app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,33 @@ class MainActivity : AppCompatActivity() {
 
         AppCenter.start(application, "8ceb9e11-db30-4574-bf74-a3f7bd735c2f",
                 Analytics::class.java, Crashes::class.java)
+
+        calculateButton.setOnClickListener {
+//            Crashes.generateTestCrash()
+           try {
+               val interestRate = interestEditText.text.toString().toFloat()
+               val currentAge = ageEditText.text.toString().toInt()
+               val retirementAge = retirementEditText.text.toString().toInt()
+               val monthly = retirementEditText.text.toString().toFloat()
+               val current = retirementEditText.text.toString().toFloat()
+
+               val properties: HashMap<String, String> = HashMap()
+               properties.put("interest_rate", interestRate.toString())
+               properties.put("current_age", currentAge.toString())
+               properties.put("retirement_age", retirementAge.toString())
+               properties.put("monthly_savings", monthly.toString())
+               properties.put("current_savings", current.toString())
+
+               if (interestRate <= 0) {
+                   Analytics.trackEvent("wrong_interest_rate", properties)
+               }
+
+               if (retirementAge <= currentAge) {
+                   Analytics.trackEvent("wrong_age", properties)
+               }
+           }  catch (ex: Exception) {
+               Analytics.trackEvent(ex.message)
+           }
+        }
     }
 }
