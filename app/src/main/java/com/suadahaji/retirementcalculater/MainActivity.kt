a0fb7788522d56b5be53da8c3d92d8f69c9f3840
Suada Haji <suadahaji@gmail.com>
2020-08-15 10:22:51 +0300

    Set label text

--- app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
+++ app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
@@ -40,6 +40,7 @@ class MainActivity : AppCompatActivity() {
                if (retirementAge <= currentAge) {
                    Analytics.trackEvent("wrong_age", properties)
                }
+               resultTextView.text = "At the current rate of $interestRate%, saving \$$monthly a month you will have \$X by $retirementAge."
            }  catch (ex: Exception) {
                Analytics.trackEvent(ex.message)
            }
