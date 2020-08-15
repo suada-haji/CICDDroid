1aa4211afdaff7628dd33ebf9e4adcc36772659f
Suada Haji <suadahaji@gmail.com>
2020-08-15 12:40:49 +0300

    Add retirement calculation

--- app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
+++ app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
@@ -7,6 +7,7 @@ import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
+import kotlin.math.pow
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,31 +19,44 @@ class MainActivity : AppCompatActivity() {
                 Analytics::class.java, Crashes::class.java)
 
         calculateButton.setOnClickListener {
-//            Crashes.generateTestCrash()
-           try {
-               val interestRate = interestEditText.text.toString().toFloat()
-               val currentAge = ageEditText.text.toString().toInt()
-               val retirementAge = retirementEditText.text.toString().toInt()
-               val monthly = retirementEditText.text.toString().toFloat()
-               val current = retirementEditText.text.toString().toFloat()
+            // Crashes.generateTestCrash()
+            try {
+                val interestRate = interestEditText.text.toString().toFloat()
+                val currentAge = ageEditText.text.toString().toInt()
+                val retirementAge = retirementEditText.text.toString().toInt()
+                val monthly = monthlySavingsEditText.text.toString().toFloat()
+                val current = currentEditText.text.toString().toFloat()
 
-               val properties: HashMap<String, String> = HashMap()
-               properties.put("interest_rate", interestRate.toString())
-               properties.put("current_age", currentAge.toString())
-               properties.put("retirement_age", retirementAge.toString())
-               properties.put("monthly_savings", monthly.toString())
-               properties.put("current_savings", current.toString())
+                val properties: HashMap<String, String> = HashMap<String, String>()
+                properties.put("interest_rate", interestRate.toString())
+                properties.put("current_age", currentAge.toString())
+                properties.put("retirement_age", retirementAge.toString())
+                properties.put("monthly_savings", monthly.toString())
+                properties.put("current_savings", current.toString())
 
-               if (interestRate <= 0) {
-                   Analytics.trackEvent("wrong_interest_rate", properties)
-               }
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate", properties)
+                }
+                if (retirementAge <= currentAge) {
+                    Analytics.trackEvent("wrong_age", properties)
+                }
 
-               if (retirementAge <= currentAge) {
-                   Analytics.trackEvent("wrong_age", properties)
-               }
-           }  catch (ex: Exception) {
-               Analytics.trackEvent(ex.message)
-           }
+                val futureSavings = calculateRetirement(interestRate, current, monthly, (retirementAge - currentAge) * 12)
+
+                resultTextView.text = "At the current rate of $interestRate%, saving \$$monthly a month you will have \$${String.format("%f", futureSavings)} by $retirementAge."
+            } catch (ex: Exception) {
+                Analytics.trackEvent(ex.message)
+            }
         }
+    }
+
+    fun calculateRetirement(interestRate: Float, currentSavings: Float, monthly: Float, numMonths: Int): Float {
+        var futureSavings = currentSavings * (1 + (interestRate / 100 / 12)).pow(numMonths)
+
+        for (i in 1..numMonths) {
+            futureSavings += monthly * (1 + (interestRate / 100 / 12)).pow(i)
+        }
+
+        return futureSavings
     }
 }
