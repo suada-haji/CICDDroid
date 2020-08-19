ddefd8f873b45407916d75d2f20bf0053574a59e
Suada Haji <suadahaji@gmail.com>
2020-08-19 08:33:30 +0300

    chore: Add sorry message after crashing

--- app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
+++ app/src/main/java/com/suadahaji/retirementcalculater/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.suadahaji.retirementcalculater
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.utils.async.AppCenterConsumer
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
 import kotlin.math.pow
@@ -18,8 +20,15 @@ class MainActivity : AppCompatActivity() {
         AppCenter.start(application, "dfd0d16a-4633-4cc9-ba3a-f006a4a69d83",
                 Analytics::class.java, Crashes::class.java)
 
+        val future = Crashes.hasCrashedInLastSession()
+        future.thenAccept(AppCenterConsumer {
+            if(it){
+                Toast.makeText(this, "Oops! Sorry about that!", Toast.LENGTH_LONG).show()
+            }
+        })
+
         calculateButton.setOnClickListener {
-            // Crashes.generateTestCrash()
+             Crashes.generateTestCrash()
             try {
                 val interestRate = interestEditText.text.toString().toFloat()
                 val currentAge = ageEditText.text.toString().toInt()
